d02239f92f745f878b8383d1632b6f5f861d430b
Egga <github@egga.de>
2019-02-05 09:39:50 +0100

    cycle 1

---
+++ src/main/java/de/egga/february/five/Thing.java
@@ -0,0 +1,4 @@
+package de.egga.february.five;
+
+public class Thing {
+}
--- src/test/java/de/egga/february/five/ThingTest.java
+++ src/test/java/de/egga/february/five/ThingTest.java
@@ -2,13 +2,11 @@ package de.egga.february.five;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 class ThingTest {
 
     @Test
     void it_should_call_for_action() {
 
-
+        new Thing();
     }
 }
