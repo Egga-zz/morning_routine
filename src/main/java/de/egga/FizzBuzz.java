f6ebea7c0531c9465b97844a7e69259487202e87
Egga <github@egga.de>
2019-02-08 09:39:32 +0100

    skateboard

--- src/main/java/de/egga/FizzBuzz.java
+++ src/main/java/de/egga/FizzBuzz.java
@@ -2,4 +2,8 @@ package de.egga;
 
 class FizzBuzz {
 
+    public String answer(Integer number) {
+
+        return number.toString();
+    }
 }
--- src/test/java/de/egga/FizzBuzzGameTest.java
+++ src/test/java/de/egga/FizzBuzzGameTest.java
@@ -1,5 +1,29 @@
 package de.egga;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class FizzBuzzGameTest {
 
+
+    @Test
+    void it_should_return_unaltered_number() {
+
+        FizzBuzz game = new FizzBuzz();
+
+        String answer = game.answer(1);
+
+        assertThat(answer).isEqualTo("1");
+    }
+
+    @Test
+    void it_should_return_another_unaltered_number() {
+
+        FizzBuzz game = new FizzBuzz();
+
+        String answer = game.answer(2);
+
+        assertThat(answer).isEqualTo("2");
+    }
 }
