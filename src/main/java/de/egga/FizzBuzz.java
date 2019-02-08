590794cd893d9af26db49fb4bc1ce6234f71de16
Egga <github@egga.de>
2019-02-08 09:32:48 +0100

    empty...

---
+++ src/main/java/de/egga/FizzBuzz.java
@@ -0,0 +1,5 @@
+package de.egga;
+
+class FizzBuzz {
+
+}
--- src/main/java/de/egga/february/five/FizzBuzzGame.java
+++
@@ -1,9 +0,0 @@
-package de.egga.february.five;
-
-class FizzBuzzGame {
-
-    String answer(Integer number) {
-
-        return number.toString();
-    }
-}
