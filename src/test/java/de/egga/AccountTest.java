5a1ca2393f26a161d940f81a55510eaf28744c3d
Egga <github@egga.de>
2019-02-18 09:45:31 +0100

    clean up test a little and call it a day :)

--- src/test/java/de/egga/AccountTest.java
+++ src/test/java/de/egga/AccountTest.java
@@ -6,29 +6,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
+    Account account = new Account();
+
     @Test
     void it_should_print_amount_in_statement() {
-        Account account = new Account();
         account.deposit(500);
-        String statement = account.printStatement();
-        assertThat(statement).contains("500");
+
+        assertThat(account.printStatement()).contains("500");
     }
 
     @Test
     void it_should_print_all_amounts_in_statement() {
-        Account account = new Account();
         account.deposit(200);
         account.deposit(300);
-        String statement = account.printStatement();
-        assertThat(statement).contains("200", "300");
+
+        assertThat(account.printStatement()).contains("200", "300");
     }
 
     @Test
     void it_should_print_balance_in_statement() {
-        Account account = new Account();
         account.deposit(200);
         account.deposit(300);
-        String statement = account.printStatement();
-        assertThat(statement).contains("500");
+
+        assertThat(account.printStatement()).contains("500");
     }
 }
