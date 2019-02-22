d9300ee56e85698761abb500ef0371360e29f897
Egga <github@egga.de>
2019-02-22 09:29:52 +0100

    g

--- src/main/java/de/egga/DateProvider.java
+++ src/main/java/de/egga/DateProvider.java
@@ -1,11 +1,17 @@
 package de.egga;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 class DateProvider {
 
     LocalDate getCurrentDate() {
 
         return LocalDate.now();
+    }
+
+    public String getCurrentDateAsString() {
+        String formattedDate = getCurrentDate().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return formattedDate;
     }
 }
