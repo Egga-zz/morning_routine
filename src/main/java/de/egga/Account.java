5c6cd344344bcf2a5439893f707f333399e280fe
Egga <github@egga.de>
2019-02-22 09:11:02 +0100

    r

--- src/main/java/de/egga/Account.java
+++ src/main/java/de/egga/Account.java
@@ -1,6 +1,7 @@
 package de.egga;
 
 public class Account {
+
     public void deposit(Integer amount) {
 
     }
--- src/test/java/de/egga/AccountTest.java
+++ src/test/java/de/egga/AccountTest.java
@@ -2,6 +2,8 @@ package de.egga;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AccountTest {
 
     @Test
@@ -9,6 +11,6 @@ public class AccountTest {
         Account account = new Account();
         account.deposit(100);
         String statement = account.printStatement();
-
+        assertThat(statement).contains("100");
     }
 }
