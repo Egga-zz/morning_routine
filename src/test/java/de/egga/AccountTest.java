6bdd5c6f36a2b2e106f01ff6a3198ea14db60d27
Egga <github@egga.de>
2019-02-22 09:08:07 +0100

    r

--- src/test/java/de/egga/AccountTest.java
+++ src/test/java/de/egga/AccountTest.java
@@ -2,32 +2,11 @@ package de.egga;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class AccountTest {
 
-    Account account = new Account();
-
     @Test
-    void it_should_print_amount_in_statement() {
-        account.deposit(500);
+    void asd() {
+        new Account();
 
-        assertThat(account.printStatement()).contains("500");
-    }
-
-    @Test
-    void it_should_print_all_amounts_in_statement() {
-        account.deposit(200);
-        account.deposit(300);
-
-        assertThat(account.printStatement()).contains("200", "300");
-    }
-
-    @Test
-    void it_should_print_balance_in_statement() {
-        account.deposit(200);
-        account.deposit(300);
-
-        assertThat(account.printStatement()).contains("500");
     }
 }
