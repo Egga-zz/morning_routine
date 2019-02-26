d136e8aa93b1572052e7336791f03d8591a63dac
Egga <github@egga.de>
2019-02-26 09:43:51 +0100

    spare, level 1

--- src/main/java/de/egga/Game.java
+++ src/main/java/de/egga/Game.java
@@ -1,10 +1,39 @@
 package de.egga;
 
+import static java.lang.Integer.parseInt;
+
 public class Game implements BowlingGameCalculator {
 
     @Override
     public Integer calculateScore(String scoreCard) {
 
-        return 1;
+        String[] rolls = scoreCard.split("");
+
+        return countPinsOf(rolls);
+    }
+
+    private int countPinsOf(String[] rolls) {
+
+        Integer pinsKnockedDownInFrame = null;
+
+        int sum = 0;
+        for (String roll : rolls) {
+
+            int pinsKnockedDownInRoll = 0;
+            if (roll.equals("/")) {
+                pinsKnockedDownInRoll = 10 - pinsKnockedDownInFrame;
+            } else {
+                pinsKnockedDownInRoll = parseInt(roll);
+            }
+
+            if (pinsKnockedDownInFrame == null) {
+                pinsKnockedDownInFrame = pinsKnockedDownInRoll;
+            } else {
+                pinsKnockedDownInFrame = null;
+            }
+
+            sum += pinsKnockedDownInRoll;
+        }
+        return sum;
     }
 }
